cc00c2c4699e1a0ce827a971480c40e9777405ec
Hanna <krnhan003@myuct.ac.za>
2020-02-26 16:56:49 +0200

    Initial

---
+++ assignment1/LSArrayApp.java
@@ -0,0 +1,80 @@
+// LS Array App
+// Name: Hanna Kuruneri
+// Student: KRNHAN003
+// 25 March 2020
+
+import java.util.*;
+import java.io.*;
+public class LSArrayApp{
+
+String[] textFile = new String[2976];
+      
+   public static void main(String args[])
+   {
+   LSArrayApp ls = new LSArrayApp();
+   ls.printAllAreas();
+ 
+   }
+        
+   public LSArrayApp() {
+   getData();
+   }
+        
+   
+   public void printAreas(String stage, String day, String startTime)
+   {
+      String key = stage+"_"+day+"_"+startTime;
+      //System.out.println(key);
+      boolean found = false;
+      for (int j = 0; j<textFile.length; j++){
+         //System.out.println(textFile[j]);
+         if (textFile[j].contains(key)){
+         System.out.println(textFile[j]);
+         found = true;
+         break;
+         }
+         
+         }
+         if (!found){
+            System.out.println("Areas not found.");
+         }
+      }
+      
+   
+   
+   public void printAllAreas()
+      {
+      for (int i =0; i<textFile.length; i++){
+         System.out.println(textFile[i]);
+      }
+   }
+
+   public void getData()
+      {
+      try 
+            {
+            File read = new File("Load_Shedding_All_Areas_Schedule_and_Map.clean.final.txt");
+            Scanner data = new Scanner(read);
+           
+               //LSArrayData area = new LSArrayData(data.nextLine); 
+               for(int i=0; i<2976; i++)
+                  {
+                  textFile[i] = data.nextLine();
+                  }                 
+               
+               data.close(); 
+           }
+          catch(Exception ex)
+            {
+            System.out.println("Error reading file.");
+            }
+      
+      }
+
+}
+// }      
+//      // public LSArrayApp() {
+//       
+//     //  }
+//  //  public void printAreas(string stage, string day, string startTim
+// 
---
+++ assignment1/LSArrayData.java
@@ -0,0 +1,26 @@
+//	LS	Array	Object
+//	Name:	Hanna	Kuruneri
+//	Student:	KRNHAN003
+//	25	March	2020
+
+import java.util.Scanner;
+import java.io.*;
+
+public class LSArrayData {
+
+   private String key = "";
+   private String stages = "";
+   String[] rowArray = new String[2];
+
+   public LSArrayData(String input) 
+   	{
+      key = input1;
+      stages = input2;
+      
+             }
+   
+   public static void main(String args[]){
+	   String[] rowArray = row.split(" ");
+      System.out.println(rowArray);
+      }
+} 
